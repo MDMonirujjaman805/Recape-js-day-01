313e1b06869b7758b5bd00611ebf33c5a53706c9
MD Monirujjaman <monirbinkhaled1@gmail.com>
2024-12-23 18:49:09 +0600

    practice done

---
+++ index.js
@@ -0,0 +1,189 @@
+// console.log("monir");
+// const name = 'Monir';
+// console.log(name);
+
+// const k = [45,65,34,35,35,34,64];
+// const a = [45,65,34,55,56,34,64];
+// const x = 8;
+// const y = true;
+// console.log(y);
+// console.log(typeof y);
+// console.log(x);
+// console.log(typeof x);
+// console.log(typeof name);
+// console.log(k);
+// console.log(a);
+// console.log(a[3])
+// console.log(k[3])
+// console.log(typeof k);
+// console.log(k.length);
+// console.log(k[3]);
+
+// const monir = {
+//   name:'Monir',
+//   age:25,
+//   student:true,
+//   maried:false,
+// };
+// console.log(monir);
+// console.log(typeof monir);
+// console.log(monir.length);
+
+
+// const s = 78;
+// const a = 68;
+// const f = 58;
+// const total = s+a+f;
+// const min = total-100;
+// console.log(min);
+
+// const j = 89;
+// const k = 99;
+// const sum = j*k;
+// const devition =sum /5;
+// console.log(devition);
+
+
+// const t = 100;
+// const modeilo = t%2;
+// console.log (typeof modeilo);
+
+// const k = 8;
+// const j = 70;
+// const devided = k/j;
+// console.log(devided);
+// console.log(devided.toFixed(5));
+// console.log(typeof devided.toFixed(2));
+
+// const x = 123.123456;
+// const formated = x.toFixed(3);
+// console.log(formated);
+// console.log(typeof formated);
+
+
+// const j = 123.456456798;
+// // const q =parseFloat(j.toFixed(2));
+// console.log(j);
+// console.log(j.toFixed(3));
+// console.log(typeof q);
+
+// const j = 123.123456789;
+// const k = parseFloat(j.toFixed(2));
+// console.log(j);
+// console.log(k);
+// const s = 12.99999999;
+// const a = parseInt(s);
+// console.log(a);
+
+// console.log(2**6)
+
+// let x = 8;
+// x/=6
+// let y = parseFloat(x.toFixed(2));
+// console.log(y);
+
+
+// comparison oparetore 
+// let x = 50;
+// let y = 5;
+// let compare = x <= y;
+// console.log(compare);
+
+// condition 
+// let age = 70;
+// if(age<18){
+//   console.log('you are Child')
+// }
+// else if(age>=18){
+//   console.log("you are an Adult")
+// }
+// else if(age>=30){
+//   console.log("you are a Yong")
+// }
+// else{
+//   console.log("you are an oldman")
+// }
+
+
+
+// let score = 85;
+
+// if (score >= 90) {
+//     console.log("Grade: A");
+// } else if (score >= 80) {
+//     console.log("Grade: B");
+// } else if (score >= 70) {
+//     console.log("Grade: C");
+// } else {
+//     console.log("Grade: F");
+// }
+
+
+// let color = 66;
+// if(color <= 10){
+//   console.log("white")
+// }
+// else if (color <= 25){
+//   console.log("red")
+// }
+// else if (color <= 50){
+//   console.log('green')
+// }
+// else if (color <= 75){
+//   console.log("blue")
+// }
+// else{
+//   console.log('gray')
+// }
+
+
+// let age = 10;
+// let message = age >= 18 ? "You are an adult." : "You are a minor.";
+// console.log(message);
+
+
+// let result = 40;
+// let position = result >= 50 ?'you are 1st':'you are 2nd';
+// console.log(position);
+
+
+// let score = 50000;
+// let grade = score >= 90 ? "A":
+//             score >= 80 ? "B":
+//             score >= 70 ? "C":
+//             score >= 60 ? "D":
+//             score >= 50 ? "E":
+//             score >= 40 ? "F":
+//             "G";
+// console.log(grade);
+
+
+// coppy to chatGPT................
+
+// let color = 500; // তুমি এখানে যে কোনো মান দিতে পারো
+
+// // Check if color is within the range of 0 to 100
+// if (color < 0 || color > 100) {
+//     console.log("Invalid value! Color should be between 0 and 100.");
+// } else if (color >= 75) {
+//     console.log("blue");
+// } else if (color >= 50) {
+//     console.log("green");
+// } else if (color >= 25) {
+//     console.log("red");
+// } else if (color >= 10) {
+//     console.log("white");
+// } else {
+//     console.log("gray");
+// }
+
+
+
+let age = 16;
+let adult = age >= 18 ? "you are an adult" : "you are not adult";
+console.log(adult);
+
+
+let single = false;
+let maried = single === true?"unmaried":"maried";
+console.log(maried);
